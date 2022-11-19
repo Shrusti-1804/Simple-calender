1a930e47833c322c3477222260a08fa35f61fef5
Shrusti-1804 <118587694+Shrusti-1804@users.noreply.github.com>
2022-11-19 21:17:45 +0530

    Create main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main()
 {
   char* months[]={"January","Febuary","March","April","May","June","July","August","September","Octeber","November","Decmber"};
   int monthDay[]={31,28,31,30,31,30,31,31,30,31,30,31};
-  int year ,month,day,daysinmonth,weekDays=0,startingDays;
+  int year ,month,day,days_in_month,week_Days=0,starting_Days,first_day;
   printf("Enter the year:");
   scanf("%d",&year);
 
@@ -14,38 +14,38 @@ int main()
    printf("\n\n           %d CALENDER      ",year);
 
   if (year%4==0&&year%100!=0)
-   monthDay[1]=29;
+   month_Day[1]=29;
 
-   startingDays=firstday(year);
+   starting_Days=first_day(year);
 
 
   for(month=0;month<12;month++)
   {
-    daysinmonth=monthDay[month];
+    days_in_month=month_Day[month];
     printf("\n\n_______________%s_______________",months[month]);
     printf("\n\n   Sun  Mon  Tue  Wed  Thu  Fri  Sat\n");
 
-    for(weekDays=0;weekDays<startingDays;weekDays++)
+    for(week_Days=0;week_Days<starting_Days;week_Days++)
     {
       printf("     ");
     }
 
-    for(day=1;day<=daysinmonth;day++)
+    for(day=1;day<=days_in_month;day++)
     {
       printf("%5d",day);
 
-      if(++weekDays>6)
+      if(++week_Days>6)
       {
         printf("\n");
-        weekDays=0;
+        week_Days=0;
       }
-      startingDays=weekDays;
+      starting_Days=week_Days;
     }
 
   }
 
 }
-int firstday(int year)
+int first_day(int year)
 {
   int day;
   day=(((year-1)*365)+((year-1)/4)-((year-1)/100)+((year)/400)+1)%7;
