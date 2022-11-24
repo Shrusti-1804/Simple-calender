70bd41e36d057a5fd39f93df7206ef232881da8d
Shrusti-1804 <118587694+Shrusti-1804@users.noreply.github.com>
2022-11-24 19:23:25 +0530

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 
 int main()
 {
-  char* months[]={"January","Febuary","March","April","May","June","July","August","September","Octeber","November","December"};
+  char* months[]={"January","February","March","April","May","June","July","August","September","Octeber","November","December"};
   int month_day[]={31,28,31,30,31,30,31,31,30,31,30,31};
   int year ,month,day,days_in_month,week_day=0,starting_day;
   printf("Enter the year:");
